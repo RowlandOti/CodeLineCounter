39dacf92773d0c2777d658a3d9cf9cca090a9e8e
Otieno Rowland <r.otieno@kokonetworks.com>
2021-03-20 09:20:46 +0300

    read tags from properties

--- lineCounterPlugin/src/main/kotlin/com/rowlandoti/lines/CodeLineCounterPlugin.kt
+++ lineCounterPlugin/src/main/kotlin/com/rowlandoti/lines/CodeLineCounterPlugin.kt
@@ -2,16 +2,27 @@ package com.rowlandoti.lines
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginConvention
 
 class CodeLineCounterPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.tasks.create("codeLines") {
-            it.doLast {
-                println("Hello from CodeLinesCounterPlugin")
+        project.tasks.create("codeLines") { task ->
+            task.doLast {
+                printCodeLinesCount(project)
             }
         }.apply {
             group = "stat"
         }
+    }
+
+    private fun printCodeLinesCount(project: Project) {
+        var totalCount = 0
+        project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets.forEach { sourceSet ->
+            sourceSet.allSource.forEach { file ->
+                totalCount += file.readLines().count()
+            }
+        }
+        println("Total lines: $totalCount")
     }
 }
